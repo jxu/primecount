f85902b01341909c0fdf53d3123aaf4280c13fb1
jxu <7989982+jxu@users.noreply.github.com>
2025-04-29 19:39:44 -0400

    Add reset test for fenwick tree

--- primecount.cpp
+++ primecount.cpp
@@ -70,20 +70,25 @@ void test_fenwick_tree()
 
     int a1[5] = {1, 2, 3, 4, 5};
 
-    for (int i=0; i < 5; ++i)
+    for (int i = 0; i < 5; ++i)
         assert(ft.sum_to(i) == a1[i]);
 
     ft.try_decrease(1);
 
     int a2[5] = {1, 1, 2, 3, 4};
 
-    for (int i=0; i<5; ++i)
+    for (int i = 0; i < 5; ++i)
         assert(ft.sum_to(i) == a2[i]);
 
     ft.try_decrease(1); // should not change
 
-    for (int i=0; i<5; ++i)
+    for (int i = 0; i < 5; ++i)
         assert(ft.sum_to(i) == a2[i]);
+
+    ft.reset();
+
+    for (int i = 0; i < 5; ++i)
+        assert(ft.sum_to(i) == a1[i]);
 }
 
 // signum: returns -1, 0, or 1
